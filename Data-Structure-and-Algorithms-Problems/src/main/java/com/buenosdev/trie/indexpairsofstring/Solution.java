aac4e2a01e4272f502004c48d0d78eaaa6ea0928
AugustoCalado <augusto.calado11@gmail.com>
2023-01-12 12:32:00 -0300

    (Trie) Problem 1: Index Pairs of a String

---
+++ Data-Structure-and-Algorithms-Problems/src/main/java/com/buenosdev/trie/indexpairsofstring/Solution.java
@@ -0,0 +1,65 @@
+package com.buenosdev.trie.indexpairsofstring;
+
+import java.util.*;
+
+public class Solution {
+
+    public int[][] indexPairs(String text, String[] words) {
+        Node root = new Node();
+        for (String word : words) {
+            add(root, word);
+        }
+
+        char[] arr = text.toCharArray();
+        int textLenght = arr.length;
+
+        List<int[]> ll = new ArrayList<>();
+        for (int i = 0; i < textLenght; i++) {
+            for (int j = i + 1; j <= textLenght; j++) {
+                String sub = text.substring(i, j);
+                if (search(root, sub)) {
+                    ll.add(new int[]{i, j - 1});
+                }
+            }
+        }
+
+        int[][] ans = new int[ll.size()][2];
+        for (int i = 0; i < ll.size(); i++) {
+            ans[i] = ll.get(i);
+        }
+
+        return ans;
+    }
+
+    public static void add(Node root, String word) {
+        Node cur = root;
+        cur.pass++;
+        char[] arr = word.toCharArray();
+        for (char c : arr) {
+            if (!cur.nexts.containsKey(c))
+                cur.nexts.put(c, new Node());
+            cur = cur.nexts.get(c);
+            cur.pass++;
+        }
+
+        cur.end++;
+    }
+
+    public static boolean search(Node root, String word) {
+        Node cur = root;
+        char[] arr = word.toCharArray();
+        for (char c : arr) {
+            if (!cur.nexts.containsKey(c)) return false;
+            cur = cur.nexts.get(c);
+        }
+
+        return cur.end > 0;
+    }
+
+    static class Node {
+        int pass = 0;
+        int end = 0;
+        Map<Character, Node> nexts = new HashMap<>();
+    }
+
+}
---
+++ Data-Structure-and-Algorithms-Problems/src/main/java/com/buenosdev/trie/indexpairsofstring/Trie.java
@@ -0,0 +1,102 @@
+package com.buenosdev.trie.indexpairsofstring;
+
+class TrieNode {
+    TrieNode[] children = new TrieNode[26]; // Children nodes
+    boolean isEndOfWord; // Flag to represent the end of a word
+
+    String letter;
+
+    public TrieNode(String letter) {
+        isEndOfWord = false;
+        for (int i = 0; i < 26; i++)
+            children[i] = null;
+
+        this.letter = letter;
+    }
+}
+
+class Trie {
+    TrieNode root;
+
+    public Trie() {
+        root = new TrieNode("");
+    }
+
+    // Recursive function to delete a key from the Trie
+    private boolean delete(TrieNode current, String word, int index) {
+        // Base case: If the word's end is reached
+        if (index == word.length()) {
+            // If the word exists, unmark it and check if the node can be deleted
+            if (current.isEndOfWord) {
+                current.isEndOfWord = false;
+                return current.children.length == 0; // Return true if no children exist
+            }
+            return false;
+        }
+
+        char ch = word.charAt(index);
+        TrieNode node = current.children[ch - 'a'];
+        if (node == null) {
+            return false; // Word not found
+        }
+
+        boolean shouldDeleteChild = delete(node, word, index + 1);
+
+        // If true is returned, delete the child reference of the current TrieNode
+        if (shouldDeleteChild) {
+            current.children[ch - 'a'] = null;
+            // Return true if no mappings are left in the map
+            return current.children.length == 0;
+        }
+
+        return false;
+    }
+
+    // Function to delete a word from the Trie
+    public void deleteWord(String word) {
+        delete(root, word, 0);
+    }
+
+
+    // Function to insert a word into the Trie
+    public void insert(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            int index = word.charAt(i) - 'a'; // Convert character to index
+            if (node.children[index] == null)
+                node.children[index] = new TrieNode(String.valueOf(word.charAt(i))); // Create a new node if it doesn't exist
+            node = node.children[index];
+        }
+        node.isEndOfWord = true; // Mark the end of the word
+    }
+
+
+    // Function to search a word in the Trie
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            int index = word.charAt(i) - 'a';
+            if (node.children[index] == null) return false; // Word not found
+            node = node.children[index];
+        }
+        return node.isEndOfWord; // Return true if word exists, false otherwise
+    }
+
+
+    // Returns if there is any word in the trie that starts with the given prefix.
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (char c : prefix.toCharArray()) {
+            if (node.children[c - 'a'] == null) return false;
+            node = node.children[c - 'a'];
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        var trie = new Trie();
+        trie.insert("thestoryofleetcodeandme");
+        var result = trie.search("leet");
+        System.out.println(result);
+    }
+}
