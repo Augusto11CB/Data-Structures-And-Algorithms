8ca53aca66f7e3d54798a88e01cf996cb7b98ff3
AugustoCalado <augusto.calado11@gmail.com>
2023-02-16 17:30:00 -0300

    (Greedy Algorithm) Problem 3: Minimum Add to Make Parentheses Valid

--- Data-Structure-and-Algorithms-Problems/src/main/java/com/buenosdev/greedy/minaddtomakevalid/ProblemSolution.java
+++ Data-Structure-and-Algorithms-Problems/src/main/java/com/buenosdev/greedy/minaddtomakevalid/ProblemSolution.java
@@ -34,6 +34,27 @@ public class ProblemSolution {
         return result + (Math.abs(rightCounter - leftCounter));
     }
 
+    public int minAddToMakeValid(String s) {
+        var sArray = s.toCharArray();
+        var leftCounter = 0;
+        var rightCounter = 0;
+        var result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (sArray[i] == '(') {
+                leftCounter = leftCounter + 1;
+            } else if (sArray[i] == ')') {
+                if (leftCounter > rightCounter) {
+                    rightCounter = rightCounter + 1;
+                } else {
+                    result = result + 1;
+                }
+            }
+        }
+
+        return result + (Math.abs(leftCounter - rightCounter));
+    }
+
     public static void main(String[] args) {
     }
 
