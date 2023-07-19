9e582e5d65067d804c87d62173b89e19f3f273e8
AugustoCalado <augusto.calado11@gmail.com>
2023-07-19 20:52:56 -0300

    LeetCode - 141. Linked List Cycle

---
+++ LeetCode-Problems/141-Linked-List-Cycle/src/main/java/com/buenosdev/LinkedListCycle.java
@@ -0,0 +1,58 @@
+package com.buenosdev;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class LinkedListCycle {
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        System.out.println(new LinkedListCycle().hasCycleV2(head));
+    }
+
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> set = new LinkedHashSet<>();
+        while (head != null) {
+            if (set.contains(head)) {
+                return true;
+            }
+            set.add(head);
+            head = head.next;
+        }
+        return false;
+    }
+
+    public boolean hasCycleV2(ListNode head) {
+        ListNode slowPointer = head;
+        ListNode fastPointer = head; // two times faster
+        while (fastPointer != null && fastPointer.next != null) {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+            if (slowPointer == fastPointer) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
