052d6d132f77e9bbf62873d0a060b8fe03127261
AugustoCalado <augusto.calado11@gmail.com>
2023-07-23 21:36:00 -0300

    LeetCode - 206 Reverse Linked List

--- LeetCode-Problems/206-Reverse-Linked-List/src/main/java/com/buenodevs/ReverseLinkedList.java
+++ LeetCode-Problems/206-Reverse-Linked-List/src/main/java/com/buenodevs/ReverseLinkedList.java
@@ -6,17 +6,41 @@ public class ReverseLinkedList {
         ListNode list = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
         ListNode reversedList = recursiveReverseList(list);
 
-        while (reversedList!= null) {
+        while (reversedList != null) {
             System.out.println(reversedList.val);
             reversedList = reversedList.next;
         }
+    }
+
+
+    public static ListNode rotateRight(ListNode head, int k) {
+        if (head == null) return null;
+        if (head.next == null) return head;
+
+        ListNode aux = head;
+        int size = 1;
+        while (aux.next != null) {
+            size++;
+            aux = aux.next;
+        }
+
+        aux.next = head;
+
+        for (int i = 0; i < size - k % size; i++) {
+            aux = aux.next;
+        }
+
+        head = aux.next;
+        aux.next = null;
+
+        return head;
     }
 
     public static ListNode reverseList(ListNode head) {
         ListNode aux = head;
         ListNode prev = null;
 
-        while (aux!= null) {
+        while (aux != null) {
             ListNode next = aux.next;
             aux.next = prev;
             prev = aux;
