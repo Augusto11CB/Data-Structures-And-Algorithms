3bbf9667a2bc3cce67eba338cc9f9623a481b48c
AugustoCalado <augusto.calado11@gmail.com>
2024-03-03 18:58:11 -0300

    (HashMap) Problem: Two Sum

---
+++ Data-Structure-and-Algorithms-Problems/src/main/java/com/buenosdev/hash/ProblemSolution.java
@@ -0,0 +1,41 @@
+package com.buenosdev.hash;
+
+import java.util.HashMap;
+
+class ProblemSolution {
+
+    // (HashMap) Problem: Two Sum
+
+    /*
+    https://leetcode.com/problems/two-sum/description/
+     * */
+
+    /*
+     * */
+
+    public static void main(String[] args) {
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        var hashComplement = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+            var complement = target - nums[i];
+
+            if (hashComplement.containsKey(complement)) {
+                return new int[]{hashComplement.get(complement), i};
+            }
+            hashComplement.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
+/*
+Time Complexity:
+ - O(n): The time complexity of this algorithm is O(n), where n is the length of the string. This is because we're
+  processing each character in the string exactly once.
+
+Space Complexity:
+ - O(n): in the worst-case scenario when all the characters in the string are opening parentheses, so we push each character
+  onto the Stack.
+*/
